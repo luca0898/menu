e5072125346028497dd85c79f09164c963de9e57
luca0898 <lucaguiraldello@hotmail.com>
2023-02-23 11:15:16 -0300

    configurando cors e ajustando código

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import express, { json } from "express";
+import cors from "cors";
 
 const hostname = process.env.hostname
 const port = process.env.port
 
 const app = express()
+
+app.use(cors({
+    allowedHeaders: "*",
+    methods: "*",
+    origin: "*",
+}))
+
 app.use(json())
 
 app.get('/healthcheck', (request, response) => {
     return response.json({ status: "UP" })
 })
 
-app.list(port, hostname, () => console.log(`Server Running at ${hostname}:${port}`))
+app.listen(port, hostname, () => console.log(`Server Running at ${hostname}:${port}`))
